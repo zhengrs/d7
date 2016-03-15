4e0f81467e366a03ca9c68f70129a7b44e5cc57d
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-15 16:42:19 -0400

    Update selection.js

--- src/selection/selection.js
+++ src/selection/selection.js
@@ -1,3 +1,20 @@
+import "../core/subclass.js"
+
+//  d7_selection
+//  d7_select
+//  d7_selectAll
+//  d7_selectionPrototype
+
+function d7_selection(groups){
+  d7_subclass(groups, d7_selectionPrototype);
+}
+
+var d7_select = function(s,n){  
+  return n.querySelector(s);  
+},
+d7_selectAll = function(s,n){
+  return n.querySelectorAll(s);
+};
 
 
 var d7_selectionPrototype = [];
