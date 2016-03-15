0880bb739a7bc8dfacf15482b41f2ad3093978ab
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-15 17:27:50 -0400

    Update select.js

--- src/selection/select.js
+++ src/selection/select.js
@@ -1,6 +1,29 @@
 import "selection";
 
 
-d7_selectionPrototype.select = function( selector ){
+d7_selectionPrototype.select = function(selector){
+  
+  var subgroups=[], subgroup, group, node, subnode;
+  
+  selector = d7_selection_selector(selector);
+  
+  for(var i=0, n=this.length; i<n; i++){
+    subgroups.push(subgroup=[]);
+    subgroup.parentNode = (group=this[i]).parentNode;
+    for(var j=0, m=group.length; j<m; j++){
+      if(node=group[j]){
+        subgroup.push(selector.call(node, node.__data__, j))
+      }
+      else{
+        subgroup.push(null);
+      }
+    }
+  }
   
 };
+
+function d7_selection_selector(selector){
+  return typeof selector === "function" ? selector : function(){
+    d7_select(selector, this);
+  };
+}
