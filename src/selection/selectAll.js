799396c394e0051b83ffcd974cff37acf03f27ae
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-15 18:10:28 -0400

    Update selectAll.js

--- src/selection/selectAll.js
+++ src/selection/selectAll.js
@@ -1,5 +1,22 @@
 import "selection.js";
 
-d7_selectionPrototype.selectAll = function( selector ){
-
+d7_selectionPrototype.selectAll = function(selector){
+  
+  var subgroups=[], subgroup, group;
+  
+  selector = d7_selection_selectorAll(selector); // selector.call(document, selector)
+  
+  for(var i=0, n=this.length; i<n; i++){
+    for(var j=0, m=(group=this[i]).length; j<m; j++){
+      subgroups.push(subgroup=[]);
+    }
+  }  
+  
+  
 };
+
+function d7_selection_selectorAll = function(selector){
+  return typeof selector === "function" ? selector : function(){
+    return this.querySelectorAll(selector);
+  };
+}
