12f0d1d8129fd1bcd67035873aa5a257e1998c07
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 15:37:27 -0400

    Update enter.js

--- src/selection/enter.js
+++ src/selection/enter.js
@@ -12,4 +12,22 @@ d7_selection_enterPrototype.append = d7_selectionPrototype.append;
 
 d7_selection_enterPrototype.select = function(selector){
   
+  var subgroups = [], subgroup, group, node, subnode;
+  
+  for(var i=0, n=this.length; i<n; i++){
+    subgroups.push( subgroup = [] );
+    subgroup.parentNode = (group=this[i]).parentNode;
+    for(var j=0, m=group.length; j<m; j++){
+      if( node=group[j] ){
+        subgroup.push( subnode = selector.call(group.parentNode, node.__data__, j) );
+        if( subnode ){
+          subnode.__data__ = node.__data__;
+        }
+      }
+      else{
+        subgroup.push(null);
+      }
+    }
+  }
+  
 };
