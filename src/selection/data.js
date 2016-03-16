267df6e1e267e2034f761dc03d93fe6672cacaf6
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 17:37:54 -0400

    Update data.js

--- src/selection/data.js
+++ src/selection/data.js
@@ -1,1 +1,24 @@
-import ""
+import "selection";
+
+d7_selectionPrototype.data = function(value){
+
+  value = typeof value === "function" ? value : function(){
+    return value;
+  };  
+  
+  var update = d7_selection([]),
+      enter = d7_selection_enter([]);
+      
+  var group;
+      
+  for( var i=0, n=this.length; i<n; i++ ){
+    bind(group = this[i], value.call(group, group)
+  }    
+      
+  function bind( group, value ){
+    
+  }
+  
+}
+
+
