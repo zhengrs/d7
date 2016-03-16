6a6c3501ebf6fae79ab477c8598f132427695c19
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 10:12:03 -0400

    Update document.js

--- src/core/document.js
+++ src/core/document.js
@@ -1,5 +1,6 @@
 var d7_document = this.document;
 
-function d7_documentElement(){
-
+// node might be : node, document, window
+function d7_documentElement(node){
+  return node && (node.ownerDocument || node || node.document).documentElement; 
 }
