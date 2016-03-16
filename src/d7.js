a5364f531a8156d618abaf47bbf3160607faa60c
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 10:35:56 -0400

    Update d7.js

--- src/d7.js
+++ src/d7.js
@@ -1,15 +1,34 @@
+import "core/";
 import "selection/";
 
 d7 = {};
 
 d7.select = function(selector){
-  var groups;
+  var groups, group;
   
   if( typeof selector === "string" ){
-    groups = d7_s
+    groups = [ group = [d7_select(selector,d7_document)] ];
   }
+  else{
+    groups = [ group = [selector] ];
+  }
+  group.parentNode = d7_documentElement(d7_document);
+  
+  return d7_selection(groups);
 };
 
 d7.selectAll = function(selector){
+  var groups, group;
   
+  if(typeof selector === "string"){
+    groups = [ group = d7_array(d7_selectAll(selector, d7_document)) ];
+  }
+  else{
+    groups = [ group = d7_array(selector) ];
+  }
+  group.parentNode = d7_documentElement(d7_document);
+  
+  return d7_selection(groups);
 };
+
+
