8b99ec540e18bf5e2e00821e560e7cf9f1bd8bf0
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 09:11:08 -0400

    Update selectAll.js

--- src/selection/selectAll.js
+++ src/selection/selectAll.js
@@ -2,17 +2,19 @@ import "selection.js";
 
 d7_selectionPrototype.selectAll = function(selector){
   
-  var subgroups=[], subgroup, group;
+  var subgroups=[], subgroup, group, node;
   
   selector = d7_selection_selectorAll(selector); // selector.call(document, selector)
   
   for(var i=0, n=this.length; i<n; i++){
     for(var j=0, m=(group=this[i]).length; j<m; j++){
-      subgroups.push(subgroup=[]);
+      if(node=group[j]){
+        subgroups.push(subgroup=d7_array(selector.call(node,node.__data__,j)));
+        subgroup.parentNode = node;
+      }
     }
   }  
-  
-  
+  return d7_selection(subgroups);
 };
 
 function d7_selection_selectorAll = function(selector){
