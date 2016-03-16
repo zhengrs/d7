1fb3aa44827afc15af44245c48ad326e211cf403
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 13:31:25 -0400

    Update append.js

--- src/selection/append.js
+++ src/selection/append.js
@@ -1,5 +1,16 @@
 import "selection.js";
 
 d7_selection_prototype.append = function(name){
-
+  
+  var creator = d7_selection_creator(name);
+  
+  return this.select(function(d,i){
+    return this.appendChild(creator.call(this, this.__data__, i));
+  });
 };
+
+function d7_selection_creator(name){
+  return typeof name === "function" ? name : function(){
+    return d7_document.createElement(name);
+  };
+}
