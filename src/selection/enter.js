16294667542d2e0d542af8370abc0e46fbc4e785
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-16 14:07:04 -0400

    Update enter.js

--- src/selection/enter.js
+++ src/selection/enter.js
@@ -1,2 +1,11 @@
 import "../core/subclass";
 import "selection";
+
+function d7_selection_enter(groups){
+  d7_subclass(groups, d7_selection_enterPrototype);
+  return groups;
+}
+
+var d7_selection_enterPrototype = [];
+
+d7_selection_enterPrototype.append = d7_selectionPrototype.append;
