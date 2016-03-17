3894d386b605e080b619b23b7ab1cf0cf9008156
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-17 15:19:28 -0400

    Update data.js

--- src/selection/data.js
+++ src/selection/data.js
@@ -2,9 +2,11 @@ import "selection";
 
 d7_selectionPrototype.data = function(value){
 
-  value = typeof value === "function" ? value : function(){
-    return value;
-  };  
+  value = typeof value === "function" ? value : (function(value){
+    return function(){
+      return value;
+    };
+  })(value);  
   
   var update = d7_selection([]),
       enter = d7_selection_enter([]);
