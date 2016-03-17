2fca426131e32ba78ac200660bcbc9d17e187819
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-17 14:44:13 -0400

    Update data.js

--- src/selection/data.js
+++ src/selection/data.js
@@ -12,13 +12,55 @@ d7_selectionPrototype.data = function(value){
   var group;
       
   for( var i=0, n=this.length; i<n; i++ ){
-    bind( group = this[i], value.call(group, group))
+    bind( group = this[i], value.call(group));    // here need to be edited later
   }    
+  
+  update.enter = function(){
+    return enter;
+  }
+  
+  return update;
       
   function bind( group, value ){
+    var n = group.length, 
+        m = value.length,
+        n0 = Math.min(m,n),
+        node,
+        nodeData,
+        i;
+        
+    var updateGroup = new Array(n), // different from d3 .
+        enterGroup = new Array(m);
     
+    for(i=0; i<n0; i++){
+      node = group[i];
+      nodeData = value[i];
+      if( node ){
+        node.__data__ = nodeData;
+        updateGroup[i] = node;
+      }
+      else{
+        enterGroup[i] = d7_selection_dataNode(nodeData);
+      }
+    }
+    
+    // fill enterGroup
+    for(; i<m; i++){
+      nodeData = value[i];
+      enterGroup[i] = d7_selection_dataNode(nodeData);
+    }
+    
+    enterGroup.parentNode = updateGroup.parentNode = group.parentNode;
+    
+    enter.push(enterGroup);
+    update.push(updateGroup);
   }
   
 }
+
+function d7_selection_dataNode(data){
+  return {__data__:data};
+}
+
 
 
