53249de3b7db4caa5d620d538a2a70c2277107c7
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-18 14:43:33 -0400

    Update node.js

--- src/selection/node.js
+++ src/selection/node.js
@@ -1,1 +1,14 @@
 import "selection";
+
+d7_selectionPrototype.node = function(){
+  var group, node;
+  
+  for(var i=0, n=this.length; i<n; i++){
+    for(var j=0, m=(group=this[i]).length; j<m; j++){
+      if(node = group[j]){
+        return node;
+      }
+    }
+  }
+  return null;
+};
