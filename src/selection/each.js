2b4136f643d68867d55e0c0b4c02993f50ab6793
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-21 14:15:51 -0400

    Update each.js

--- src/selection/each.js
+++ src/selection/each.js
@@ -1,2 +1,16 @@
 import "selection";
 
+d7_selectionPrototype.each = function(callback){
+  
+  var group, node;
+  
+  for(var i=0, n=this.length; i<n; i++){
+    for(var j=0, m=(group = this[i]).length; j<m; j++ ){
+      if(node=group[j]){
+        callback.call(node, node.__data__, j, i);
+      }
+    }
+  }
+  
+  return this;
+};
