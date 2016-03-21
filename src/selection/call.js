790f9f80e5ed81115cc4bf7d52f7db43cff0da6f
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-21 07:33:04 -0400

    Update call.js

--- src/selection/call.js
+++ src/selection/call.js
@@ -1,2 +1,10 @@
+import "../core/array";
 import "selection";
 
+d7_selectionPrototype.call = function(callback){
+  
+  var args = d7_array(arguments);
+  
+  
+  callback.apply(this, args.split(1));
+};
