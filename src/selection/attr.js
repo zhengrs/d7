131aac9a67dc0a09113342862ab7e71dc746b72b
Zhizunbao <zhengrs@g.clemson.edu>
2016-03-22 10:39:38 -0400

    Update attr.js

--- src/selection/attr.js
+++ src/selection/attr.js
@@ -4,18 +4,46 @@ import "selection";
 // Element.getAttribute(name)
 d7_selectionPrototype.attr = function(name, value){
   
-  var node;
-  
   if(arguments.length < 2){
-    if(node = this.node()){
-      return node.
+    if( typeof name === "string" ){
+      var node = this.node();
+      return node.getAttribute(name) :
     }
+    // todo the case that name is an object
   }
   else{
-    if(this.node
-    
-    
+    return this.each(d7_selection_attr(name, value));
+  }
+};
+
+// return function with the this object as each node element
+function d7_selection_attr(name, value){
+  
+  // value === null
+  function attrNull(){
+    this.removeAttribute(name);
   }
   
+  // typeof value === "string"
+  function attrConstant(){
+    this.setAttribute(name, value);
+  }
   
-};
+  // typoef value === "function"
+  function attrFunction(){
+    this.setAttribute( name, value.apply(this,arguments));
+  }
+  
+  return typeof value === "function" ? attrFunction : (
+      typeof value === "string" ? attrConstant : attrNull
+    );
+}
+
+
+
+
+
+
+
+
+
